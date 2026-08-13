0eebfa064855fc3396e335668211e2342153534f
TeacherEvan <ewiebotha@gmail.com>
2026-08-13 21:55:32 +0700

    chore: add .gitignore, untrack node_modules/dist, update config + capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/core';
+
+const config: CapacitorConfig = {
+  appId: 'com.ewaldt.superstudent',
+  appName: 'Super Student',
+  webDir: 'dist'
+};
+
+export default config;
